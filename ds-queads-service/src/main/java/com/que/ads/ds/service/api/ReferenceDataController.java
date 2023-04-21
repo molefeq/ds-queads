aa5e8556afe1d6c05770aa6020ca59542561d04b
Qinisela Molefe <molefeq@gmail.com>
2023-04-21 20:37:02 +0200

    Adding swagger

--- ds-queads-service/src/main/java/com/que/ads/ds/service/api/ReferenceDataController.java
+++ ds-queads-service/src/main/java/com/que/ads/ds/service/api/ReferenceDataController.java
@@ -6,6 +6,7 @@ import com.que.ads.ds.data.model.response.SubCategoryModel;
 import com.que.ads.ds.service.service.CategoryService;
 import com.que.ads.ds.service.service.ProvinceService;
 import com.que.ads.ds.service.service.SubCategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 //import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,36 +23,43 @@ public class ReferenceDataController {
     private final SubCategoryService subCategoryService;
 
     @GetMapping("/provinces")
+    @Operation(description = "get all provinces.")
     public List<ProvinceModel> provinces() {
         return provinceService.getProvinces();
     }
 
     @GetMapping("/categories")
+    @Operation(description = "get all categories.")
     public List<CategoryModel> categories() {
         return categoryService.getCategories(true);
     }
 
     @GetMapping("/categories/city/{cityId}")
+    @Operation(description = "get all categories with the policy count filtered by the provided city.")
     public List<CategoryModel> categoriesByCity(@PathVariable int cityId) {
         return categoryService.getCategoriesForCity(cityId);
     }
 
     @GetMapping("/categories/province/{provinceId}")
+    @Operation(description = "get all provinces with the policy count filtered by the provided city.")
     public List<CategoryModel> categoriesByProvince(@PathVariable int provinceId) {
         return categoryService.getCategoriesForProvince(provinceId);
     }
 
     @GetMapping("/sub-categories/{categoryId}")
+    @Operation(description = "get all sub categories the belong to the selected category.")
     public List<SubCategoryModel> subCategories(@PathVariable int categoryId) {
         return subCategoryService.getSubCategories(categoryId);
     }
 
     @GetMapping("/sub-categories/{categoryId}/city/{cityId}")
+    @Operation(description = "get all sub categories the belong to the selected category with the policy count filtered by the provided city.")
     public List<SubCategoryModel> subCategoriesByCity(@PathVariable int categoryId, @PathVariable int cityId) {
         return subCategoryService.getSubCategoriesForCity(categoryId, cityId);
     }
 
     @GetMapping("/sub-categories/{categoryId}/province/{provinceId}")
+    @Operation(description = "get all sub categories the belong to the selected category with the policy count filtered by the provided province.")
     public List<SubCategoryModel> subCategoriesByProvince(@PathVariable int categoryId, @PathVariable int provinceId) {
         return subCategoryService.getSubCategoriesForProvince(categoryId, provinceId);
     }
